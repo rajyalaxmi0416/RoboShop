b748f9a14ce7241f62e0a7878cd599459f35e954
rajyalaxmi0416 <rajyalaxmi0416@gmail.com>
2026-01-20 23:50:52 +0530

    roboshop code

--- Frontend.sh
+++ Frontend.sh
@@ -5,5 +5,4 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
---systemctl restart nginx
 systemctl restart nginx
