6dd98e4004f699f916db1cbef41a6d489b97ab29
rajyalaxmi0416 <rajyalaxmi0416@gmail.com>
2026-01-20 22:39:11 +0530

    roboshop code

---
+++ Frontend.sh
@@ -0,0 +1,8 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+systemctl restart nginx
